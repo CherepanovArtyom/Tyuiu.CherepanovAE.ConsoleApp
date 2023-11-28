12dc70f577083424e7111e7aa3ac6467bd3cb9e8
Archi <Archi@DESKTOP-7C03C7O>
2023-11-28 18:27:25 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.CherepanovAE.ConsoleApp.Lab0.V0/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovAE.ConsoleApp.Lab0.V0.Lib;
+namespace Tyuiu.CherepanovAE.ConsoleApp.Lab0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите значение переменной K");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной W");
+            int w = Convert.ToInt32(Console.ReadLine());
+
+            DataService solve = new DataService();
+
+            var result = solve.Add(k, w);
+
+            Console.WriteLine("Сумма K + W = " + result);
+
+            var result1 = solve.Minus(k, w);
+
+            Console.WriteLine("Разность K - W = " + result1);
+
+            var result2 = solve.Mult(k, w);
+
+            Console.WriteLine("Произведение K * W = " + result2);
+
+            var result3 = solve.Div(k, w);
+
+            Console.WriteLine("Отношение K / W = " + result3);
+
+            if (result > 0)
+            {
+                Console.WriteLine("Сумма " + result + " положительная");
+            }
+            else if (result < 0)
+            {
+                Console.WriteLine("Сумма " + result + " отрицательная");
+            }
+            else if (result == 0)
+            {
+                Console.WriteLine("Сумма " + result + " равна нулю");
+            }
+
+            //int x = 1;
+            //int y = 1; 
+
+            //if (x>0 && y>0 )
+            //{
+            //    Console.WriteLine("Точка в ПЕРВОЙ четверти " );
+            //}
+
+            //else if (x < 0 && y > 0)
+            //{
+            //    Console.WriteLine("Точка во ВТОРОЙ четверти ");
+            //}
+
+            //else if (x < 0 && y < 0)
+            //{
+            //    Console.WriteLine("Точка в ТРЕТЬЕЙ четверти ");
+            //}
+
+            //else if (x > 0 && y < 0)
+            //{
+            //    Console.WriteLine("Точка в ЧЕТВЁРТОЙ четверти ");
+            //}
+
+            //else if (x == 0 && y!=0)
+            //{
+            //    Console.WriteLine("Точка лежит на оси ОРДИНАТ ");
+            //}
+
+            //else if (x != 0 && y == 0)
+            //{
+            //    Console.WriteLine("Точка лежит на оси АБЦИСС ");
+            //}
+
+            //else if (x == 0 && y == 0)
+            //{
+            //    Console.WriteLine("Точка является началом координат ");
+            //}
+
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.CherepanovAE.ConsoleApp.Lab1.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.CherepanovAE.ConsoleApp.Lab1.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.CherepanovAE.ConsoleApp.Lab1.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9aab57d5-5851-454a-900f-7f1b43d0f07a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
